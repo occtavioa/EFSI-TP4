e51f6273745a7b31c33ba8a19c915516dbe3b383
OCTAVIO ARFA TENENBERG <47292048@ort>
2023-05-11 09:49:45 -0300

    props

--- src/Componentes/Cita/Cita.js
+++ src/Componentes/Cita/Cita.js
@@ -1,13 +1,13 @@
 import './Cita.css';
 
-function Cita({mascota, dueño, fecha, hora, sintomas}) {
+function Cita(props) {
   return (
     <div className='cita'>
-        <p>Mascota: <span>{mascota}</span></p>
-        <p>Dueño: <span>{dueño}</span></p>
-        <p>Fecha: <span>{fecha.split('T')[0]}</span></p>
-        <p>Hora: <span>{hora.slice(0,5)}</span></p>
-        <p>Sintomas: <span>{sintomas}</span></p>
+        <p>Mascota: <span>{props.mascota}</span></p>
+        <p>Dueño: <span>{props.dueño}</span></p>
+        <p>Fecha: <span>{props.fecha.split('T')[0]}</span></p>
+        <p>Hora: <span>{props.hora.slice(0,5)}</span></p>
+        <p>Sintomas: <span>{props.sintomas}</span></p>
         <button className="button elimnar u-full-width">Eliminar ×</button>
     </div>
   );
--- src/Componentes/Formulario/Input/Input.js
+++ src/Componentes/Formulario/Input/Input.js
@@ -1,10 +1,10 @@
 import './Input.css'
 
-function Input({l, t, n, p}) {
+function Input(props) {
     return (
         <>
-        <label>{l}</label>
-        <input type={t} name={n} className="u-full-width" placeholder={p}></input>
+        <label>{props.l}</label>
+        <input type={props.t} name={props.n} className="u-full-width" placeholder={props.p}></input>
         </>
     )
 }
